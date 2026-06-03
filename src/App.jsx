3da627f4fcf9f2f1b03c948c76582b8098009eea
Hari L <130486345+HariLoganathan78@users.noreply.github.com>
2026-06-03 12:26:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./style.css";
+
+function App() {
+  return (
+    <div className="hero">
+
+      <nav className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <div className="content">
+        <h1>
+          Hari <br />
+          Loganathan
+        </h1>
+
+        <h2>Software Developer</h2>
+
+        <div className="social-icons">
+          <a href="https://github.com/HariLoganathan78">🐱</a>
+          <a href="#">💼</a>
+          <a href="#">📧</a>
+        </div>
+      </div>
+
+      <div className="arrow">
+        ⌄
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
